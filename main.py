fb11a16dcacd35d7e03fce07648b692a3dab59c6
Ada <adayke10@gmail.com>
2025-09-25 08:47:32 -0700

    6 trackbars

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ import eyw
 print ("Save your original image in the same folder as this program.")
 filename_valid = False
 while filename_valid == False:
-    filename = input("Enter the name of your file, including the "\
-                                 "extension, and then press 'enter': ")
+    # filename = input("Enter the name of your file, including the "\
+    #                              "extension, and then press 'enter': ")
+    filename = "picture.jpg"
     if os.path.isfile(filename) == True:
         filename_valid = True
     else:
@@ -40,51 +41,56 @@ yellow_paper = eyw.create_colored_paper(original_image, 0,255,255)
 green_paper = eyw.create_colored_paper(original_image, 0,255,0)
 
 # create grayscale trackbar
-cv2.createTrackbar('red-yellow', 'Grayscale Trackbar', 200, 255, lambda x:None)
-cv2.createTrackbar('yellow-green', 'Grayscale Trackbar', 100, 255, lambda x:None)
+cv2.createTrackbar('1', 'Grayscale Trackbar', 50, 255, lambda x:None)
+cv2.createTrackbar('2', 'Grayscale Trackbar', 100, 255, lambda x:None)
+cv2.createTrackbar('3', 'Grayscale Trackbar', 150, 255, lambda x:None)
+cv2.createTrackbar('4', 'Grayscale Trackbar', 200, 255, lambda x:None)
+cv2.createTrackbar('5', 'Grayscale Trackbar', 250, 255, lambda x:None)
 
 # instructions
 print("After clicking on a window, use the following keyboard shortcuts:")
 print("  's' to Save")
 print("  'Esc' to Exit")
 
-# color function
-def getColor(r, g, b, break1, break2):
-    paper = eyw.create_colored_paper(original_image, r, g, b)
+colors = [[0, 0, 255], [0, 255, 255], [0, 255, 0], [100, 0, 255], [100, 255, 255], [0, 255, 100]]
 
+# color function
+def getColor(r, g, b, b1, b2):
+    paper = eyw.create_colored_paper(original_image, r, g, b)
+    min = [b1+1, b1+1, b1+1]
+    max = [b2, b2, b2]
+    mask = eyw.create_mask(grayscale_image, min, max)
+    colored_parts = eyw.apply_mask(paper, mask)
+    return colored_parts
 
 # keyboard controls
 keypressed = 1
 while keypressed != 27 and keypressed != ord('s'):
     # color ratios
-    break1 = cv2.getTrackbarPos('red-yellow', 'Grayscale Trackbar')
-    break2 = cv2.getTrackbarPos('yellow-green', 'Grayscale Trackbar')
+    break1 = cv2.getTrackbarPos('1', 'Grayscale Trackbar')
+    break2 = cv2.getTrackbarPos('2', 'Grayscale Trackbar')
+    break3 = cv2.getTrackbarPos('3', 'Grayscale Trackbar')
+    break4 = cv2.getTrackbarPos('4', 'Grayscale Trackbar')
+    break5 = cv2.getTrackbarPos('5', 'Grayscale Trackbar')
 
-    redmin = [0, 0, 0]
-    redmax = [break1, break1, break1]
-    yellowmin = [break1 + 1, break1 + 1, break1 + 1]
-    yellowmax = [break2, break2, break2]
-    greenmin = [break2 + 1, break2 + 1, break2 + 1]
-    greenmax = [255, 255, 255]
-
-    # create masks for each color
-    red_mask = eyw.create_mask(grayscale_image, redmin, redmax)
-    yellow_mask = eyw.create_mask(grayscale_image, yellowmin, yellowmax)
-    green_mask = eyw.create_mask(grayscale_image, greenmin, greenmax)
-
-    # apply mask on top of colored background
-    red_parts = eyw.apply_mask(red_paper, red_mask)
-    yellow_parts = eyw.apply_mask(yellow_paper, yellow_mask)
-    green_parts = eyw.apply_mask(green_paper, green_mask)
+    red = getColor(colors[0][0], colors[0][1], colors[0][2], -1, break1)
+    orange = getColor(colors[1][0], colors[1][1], colors[1][2], break1, break2)
+    yellow = getColor(colors[2][0], colors[2][1], colors[2][2], break2, break3)
+    green = getColor(colors[3][0], colors[3][1], colors[3][2], break3, break4)
+    blue = getColor(colors[4][0], colors[4][1], colors[4][2], break4, break5)
+    purple = getColor(colors[5][0], colors[5][1], colors[5][2], break5, 255)
 
     # combines colors into single image
-    customized_image1 = eyw.combine_images(red_parts, yellow_parts)
-    customized_image2 = eyw.combine_images(customized_image1, green_parts)
+    customized_image1 = eyw.combine_images(red, orange)
+    customized_image2 = eyw.combine_images(customized_image1, yellow)
+    customized_image3 = eyw.combine_images(customized_image2, green)
+    customized_image4 = eyw.combine_images(customized_image3, blue)
+    customized_image = eyw.combine_images(customized_image4, purple)
 
     # cv2.imshow('Red Parts', red_parts)
     # cv2.imshow('Yellow Parts', yellow_parts)
     # cv2.imshow('Green Parts', green_parts)
-    cv2.imshow('Customized Image', customized_image2)
+    cv2.imshow('Customized Image', customized_image)
 
     # keyboard input
     keypressed = cv2.waitKey(1)
